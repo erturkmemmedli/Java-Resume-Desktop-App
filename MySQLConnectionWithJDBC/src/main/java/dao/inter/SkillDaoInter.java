c5b1880039f5200b81f37c537fc4e45848dc75b6
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-03 23:14:51 +0400

    Delete MySQLConnectionWithJDBC directory

--- MySQLConnectionWithJDBC/src/main/java/dao/inter/SkillDaoInter.java
+++
@@ -1,9 +0,0 @@
-package dao.inter;
-
-import entitiy.Skill;
-
-import java.util.List;
-
-public interface SkillDaoInter {
-    public List<Skill> getAll();
-}
--- MySQLConnectionWithJDBC/src/main/java/dao/inter/UserSkillDaoInter.java
+++
@@ -1,9 +0,0 @@
-package dao.inter;
-
-import entitiy.UserSkill;
-
-import java.util.List;
-
-public interface UserSkillDaoInter {
-    public List<UserSkill> getAllSkillByUserId(int userId);
-}
